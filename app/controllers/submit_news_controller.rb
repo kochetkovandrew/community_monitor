c9bdf9b05817cd89402f2e10cc6c5a57e6fc10c7
Andrew Kochetkov <kochetkov.andrew@gmail.com>
2017-12-13 19:44:32 +0100

    submit news

--- app/controllers/submit_news_controller.rb
+++ app/controllers/submit_news_controller.rb
@@ -1,8 +1,16 @@
 class SubmitNewsController < ApplicationController
 
   layout 'blank'
+  after_action :allow_iframe
 
   def new
 
   end
+
+  private
+
+  def allow_iframe
+    response.headers.except! 'X-Frame-Options'
+  end
+
 end
