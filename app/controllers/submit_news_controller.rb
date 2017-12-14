13a05f4b86df72452bd1565213a19c3175b678a0
Andrew Kochetkov <kochetkov.andrew@gmail.com>
2017-12-14 12:49:01 +0100

    submit news

--- app/controllers/submit_news_controller.rb
+++ app/controllers/submit_news_controller.rb
@@ -1,3 +1,5 @@
+require 'digest'
+
 class SubmitNewsController < ApplicationController
 
   layout 'blank'
@@ -11,6 +13,14 @@ class SubmitNewsController < ApplicationController
 
   def allow_iframe
     response.headers["X-FRAME-OPTIONS"] = "ALLOW-FROM https://api.vk.com"
+  end
+
+  def check_auth_key
+    # api_id, viewer_id, group_id, auth_key
+    secret = params[:api_id] + '_' + params[:viewer_id] + '_' + Settings.vk.submit_news.secret_key
+    if params[:auth_key] != Digest::MD5.hexdigest(secret)
+      render 'blank'
+    end
   end
 
 end
