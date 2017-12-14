f8ff9dd56ea4164936c04f63c43f42aa942913ae
Andrew Kochetkov <kochetkov.andrew@gmail.com>
2017-12-14 13:22:24 +0100

    submit news

--- app/controllers/submit_news_controller.rb
+++ app/controllers/submit_news_controller.rb
@@ -18,6 +18,7 @@ class SubmitNewsController < ApplicationController
   def check_auth_key
     # api_id, viewer_id, group_id, auth_key
     secret = params[:api_id] + '_' + params[:viewer_id] + '_' + Settings.vk.submit_news.secret_key
+    @access_token = params[:access_token]
     if params[:auth_key] != Digest::MD5.hexdigest(secret)
       render 'blank'
     end
