65f6f9c6dccbd0775d86956d6ac1319a357d040c
Andrew Kochetkov <kochetkov.andrew@gmail.com>
2017-12-14 13:29:40 +0100

    submit news

--- app/controllers/submit_news_controller.rb
+++ app/controllers/submit_news_controller.rb
@@ -4,9 +4,10 @@ class SubmitNewsController < ApplicationController
 
   layout 'blank'
   after_action :allow_iframe
+  before_filter :check_auth_key
 
   def new
-
+    @access_token = params[:access_token]
   end
 
   private
@@ -18,7 +19,6 @@ class SubmitNewsController < ApplicationController
   def check_auth_key
     # api_id, viewer_id, group_id, auth_key
     secret = params[:api_id] + '_' + params[:viewer_id] + '_' + Settings.vk.submit_news.secret_key
-    @access_token = params[:access_token]
     if params[:auth_key] != Digest::MD5.hexdigest(secret)
       render 'blank'
     end
