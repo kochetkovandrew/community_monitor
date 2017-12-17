3b335b245d1a88b0667eb504a7110e1b6df316b5
Andrew Kochetkov <kochetkov.andrew@gmail.com>
2017-12-17 21:53:30 +0100

    nonprogressors

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,7 +1,7 @@
 class TestController < ApplicationController
 
   def test
-    Rails.logger.debug self.request.env.to_yaml
+    Rails.logger.debug self.request.clean_env.to_yaml
     redirect_to 'https://spid.center/ru/articles/899'
   end
 
