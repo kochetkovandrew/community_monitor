7050200c8130494d06625e85098f7cc0c60e3290
Andrew Kochetkov <kochetkov.andrew@gmail.com>
2017-12-17 21:56:38 +0100

    nonprogressors

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,7 +1,7 @@
 class TestController < ApplicationController
 
   def test
-    Rails.logger.debug self.request.env.select{|k,v| k != 'rack.session'}.to_yaml
+    # Rails.logger.debug self.request.env.select{|k,v| k != 'rack.session'}.to_yaml
     redirect_to 'https://spid.center/ru/articles/899'
   end
 
