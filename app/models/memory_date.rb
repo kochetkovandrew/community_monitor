1af5143a1968b76aff06031a1f5e253d58441088
Andrew Kochetkov <kochetkov.andrew@gmail.com>
2018-02-16 16:17:39 +0100

    memory dates

---
+++ app/models/memory_date.rb
@@ -0,0 +1,5 @@
+class MemoryDate < ActiveRecord::Base
+  validates :day, presence: true, numericality: { only_integer: true, less_than_or_equal_to: 31, greater_than_or_equal_to: 1 }
+  validates :month, presence: true, numericality: { only_integer: true, less_than_or_equal_to: 12, greater_than_or_equal_to: 1 }
+  validates :year, numericality: { only_integer: true, allow_blank: true }
+end
