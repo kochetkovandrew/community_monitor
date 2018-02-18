03b056ce01a3827b75bfc0a634c2d80a0d693b28
Andrew Kochetkov <kochetkov.andrew@gmail.com>
2018-02-19 00:02:24 +0100

    druginteractions

--- app/controllers/art_drugs_controller.rb
+++ app/controllers/art_drugs_controller.rb
@@ -69,6 +69,6 @@ class ArtDrugsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def art_drug_params
-      params.require(:art_drug).permit([:name, :translation])
+      params.require(:art_drug).permit([:name, :translation, :hidden_name])
     end
 end
