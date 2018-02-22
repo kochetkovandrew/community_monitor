bb802cb60d8cacb51c7d849a475817d18247c087
Andrew Kochetkov <kochetkov.andrew@gmail.com>
2018-02-22 10:49:21 +0100

    copy dialogs

---
+++ db/migrate/20180221222104_create_copy_messages.rb
@@ -0,0 +1,12 @@
+class CreateCopyMessages < ActiveRecord::Migration
+  def change
+    create_table :copy_messages do |t|
+      t.references :copy_dialog
+      t.integer :user_vk_id
+      t.text :body
+      t.text :raw
+
+      t.timestamps null: false
+    end
+  end
+end
