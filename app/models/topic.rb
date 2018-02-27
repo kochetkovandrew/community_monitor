2304d969f476ed3cdb9ecb26542d96068a591e41
Andrew Kochetkov <kochetkov.andrew@gmail.com>
2018-02-27 16:09:21 +0100

    topic renew

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -8,8 +8,8 @@ class Topic < ActiveRecord::Base
     begin
       rest = 1
       step = 0
-      new_found = false
       while rest > 0
+        new_found = false
         comments_chunk = vk_lock { vk.board.get_comments(group_id: community.vk_id, topic_id: vk_id, count: step_size, offset: step*step_size, need_likes: 1, sort: (handled ? 'desc' : 'asc')) }
         if step == 0
           rest = comments_chunk[:count] - step_size
