4779ede2e01ba447cd5a60589d77c05ff155be29
Andrew Kochetkov <kochetkov.andrew@gmail.com>
2018-03-09 13:34:15 +0100

    pluralization

--- config/locales/plurals.rb
+++ config/locales/plurals.rb
@@ -7,9 +7,8 @@
                  :one
                else
                  if [2, 3, 4].include?(n % 10) &&
-                   ![12, 13, 14].include?(n % 100) &&
-                   ![22, 23, 24].include?(n % 100)
-
+                    ![12, 13, 14].include?(n % 100) #&&
+                   # ![22, 23, 24].include?(n % 100)
                    :few
                  else
                    :other
