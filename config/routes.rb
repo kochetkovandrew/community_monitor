109835eb8d630019c342ebd3d0284be25e61d89d
Andrew Kochetkov <kochetkov.andrew@gmail.com>
2018-11-05 00:08:55 +0100

    Revert "vk style"
    This reverts commit 121307048305e5b5f8ec91440e6d56593fd05c4f.

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
   resources :communities
   resources :news_requests
   resources :submit_news
-  get '/posts/vk/:id' => 'posts#vk_view'
   resources :posts
   resources :topics
 
