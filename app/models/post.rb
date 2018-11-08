2e2ea5d47c6536f602f6788790bc2d0b3affc3b7
Andrew Kochetkov <kochetkov.andrew@gmail.com>
2018-11-08 22:47:35 +0100

    get likes

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ActiveRecord::Base
 
   def get_likes(force = false, vk_client = nil)
     vk = (vk_client || VkontakteApi::Client.new(Settings.vk.user_access_token))
-    owner_id = (community_id.nil? ? member.vk_id : -commuinity.vk_id)
+    owner_id = (community_id.nil? ? member.vk_id : -community.vk_id)
     step_size = 1000
     if (raw['likes']['count'] > 0) || force
       if likes.nil? || (likes.count != raw['likes']['count']) || force
