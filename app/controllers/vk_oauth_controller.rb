afecbe42c32ae0933b7930d188de3bf84d213e84
Andrew Kochetkov <kochetkov.andrew@gmail.com>
2019-02-06 14:29:55 +0100

    oauth

--- app/controllers/vk_oauth_controller.rb
+++ app/controllers/vk_oauth_controller.rb
@@ -18,19 +18,21 @@ class VkOauthController < ApplicationController
     uri = URI(url)
     response = Net::HTTP.get(uri)
     json = JSON.parse(response)
-    member = Member.find_by_vk_id json['user_id']
-    if !member
-      member = Member.new(screen_name: json['user_id'])
-      member_hash = member.get_from_vk
-      member.set_from_vk(member_hash)
-      member.is_monitored = false
-      member.is_handled = false
-      member.save
-    end
-    session[:current_user_name] = member.full_name
-    user = User.where(vk_id: json['user_id']).first
-    if user
-      sign_in(user)
+    if json['user_id']
+      member = Member.find_by_vk_id json['user_id']
+      if !member
+        member = Member.new(screen_name: json['user_id'])
+        member_hash = member.get_from_vk
+        member.set_from_vk(member_hash)
+        member.is_monitored = false
+        member.is_handled = false
+        member.save
+      end
+      session[:current_user_name] = member.full_name
+      user = User.where(vk_id: json['user_id']).first
+      if user
+        sign_in(user)
+      end
     end
     redirect_to '/'
   end
